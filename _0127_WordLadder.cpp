88be3549750612ca66935bf4e7253b06392c623d
Rui Liu <liurui1217@gmail.com>
2019-12-27 17:04:50 -0500

    Update _0127_WordLadder.cpp

--- _0127_WordLadder.cpp
+++ _0127_WordLadder.cpp
@@ -1,5 +1,6 @@
 #include "headers.hpp"
-
+#include <unordered_set>
+#include <queue>
 struct IndexNode {
     int idx;
     IndexNode* next;
@@ -112,4 +113,75 @@ int LeetCode::_0127_Wordladder::ladderLength(std::string beginWord, std::string 
     }
 }
 
+// BFS in one direction
+static int ladderLength(std::string beginWord, std::string endWord, std::vector<std::string>& wordList) {
+    std::unordered_set<std::string> dict(wordList.begin(), wordList.end());
+    std::queue<std::string> todo;
+    todo.push(beginWord);
+    int ladder = 1;
+    while (!todo.empty()) {
+        int n = todo.size();
+        for (int i = 0; i < n; i++) {
+            std::string word = todo.front();
+            todo.pop();
+            if (word == endWord) {
+                return ladder;
+            }
+            dict.erase(word); // The met words should be removed from the dictionary
+            for (int j = 0; j < word.size(); j++) {
+                char c = word[j];
+                for (int k = 0; k < 26; k++) {
+                    word[j] = 'a' + k;
+                    if (dict.find(word) != dict.end()) {
+                        todo.push(word);
+                    }
+                }
+                word[j] = c;
+            }
+        }
+        ladder++;
+    }
+    return 0;
+}
 
+
+// BFS in dual directions
+static int ladderLength(std::string beginWord, std::string endWord, std::vector<std::string>& wordList) {
+    std::unordered_set<std::string> dict(wordList.begin(), wordList.end()), head, tail, *phead, *ptail;
+    if (dict.find(endWord) == dict.end()) {
+        return 0;
+    }
+    head.insert(beginWord);
+    tail.insert(endWord);
+    int ladder = 2;
+    while (!head.empty() && !tail.empty()) {
+        if (head.size() < tail.size()) {
+            phead = &head;
+            ptail = &tail;
+        } else {
+            phead = &tail;
+            ptail = &head;
+        }
+        std::unordered_set<std::string> temp;
+        for (auto it = phead -> begin(); it != phead -> end(); it++) {    
+            std::string word = *it;
+            for (int i = 0; i < word.size(); i++) {
+                char t = word[i];
+                for (int j = 0; j < 26; j++) {
+                    word[i] = 'a' + j;
+                    if (ptail -> find(word) != ptail -> end()) {
+                        return ladder;
+                    }
+                    if (dict.find(word) != dict.end()) {
+                        temp.insert(word);
+                        dict.erase(word);
+                    }
+                }
+                word[i] = t;
+            }
+        }
+        ladder++;
+        phead -> swap(temp);
+    }
+    return 0;
+}
