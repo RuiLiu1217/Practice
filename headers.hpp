f3b10daf358e46bd6592c601393060e77c15be91
Rui Liu <liurui1217@gmail.com>
2020-01-05 20:03:04 -0500

    update

--- headers.hpp
+++ headers.hpp
@@ -6322,6 +6322,38 @@ public:
     std::vector<int> getAllElements(TreeNode<int>* root1, TreeNode<int>* root2);
 };
 
+/*
+Given a string s formed by digits ('0' - '9') and '#' . We want to map s to English 
+lowercase characters as follows:
+
+Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+Characters ('j' to 'z') are represented by ('10#' to '26#') respectively. 
+Return the string formed after mapping.
+It's guaranteed that a unique mapping will always exist.
+
+Input: s = "10#11#12"
+Output: "jkab"
+Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+
+Input: s = "1326#"
+Output: "acz"
+
+Input: s = "25#"
+Output: "y"
+
+Input: s = "12345678910#11#12#13#14#15#16#17#18#19#20#21#22#23#24#25#26#"
+Output: "abcdefghijklmnopqrstuvwxyz"
+
+Constraints:
+1 <= s.length <= 1000
+s[i] only contains digits letters ('0'-'9') and '#' letter.
+s will be valid string such that mapping is always possible.
+*/
+class _1309_DecryptStringFromAlphabetToIngeterMapping {
+public:
+    std::string freqAlphabets(std::string s);
+};
+
 } // namespace LeetCode
 
 
