1c7a95b3a571c1a7aa7478923d9a98cb8795284c
Rui Liu <liurui1217@gmail.com>
2020-01-06 11:20:37 -0500

    Update headers.hpp

--- headers.hpp
+++ headers.hpp
@@ -2885,6 +2885,50 @@ public:
 };
 
 /*
+Given an undirected graph, return true if and only if it is bipartite.
+Recall that a graph is bipartite if we can split it's set of nodes into 
+two independent subsets A and B such that every edge in the graph has 
+one node in A and another node in B.
+
+The graph is given in the following form: graph[i] is a list of indexes j
+for which the edge between nodes i and j exists.  Each node is an integer 
+between 0 and graph.length - 1.  There are no self edges or parallel edges: 
+graph[i] does not contain i, and it doesn't contain any element twice.
+
+Input: [[1,3], [0,2], [1,3], [0,2]]
+Output: true
+Explanation: 
+The graph looks like this:
+0----1
+|    |
+|    |
+3----2
+We can divide the vertices into two groups: {0, 2} and {1, 3}.
+
+Input: [[1,2,3], [0,2], [0,1,3], [0,2]]
+Output: false
+Explanation: 
+The graph looks like this:
+0----1
+| \  |
+|  \ |
+3----2
+We cannot find a way to divide the set of nodes into two independent subsets.
+
+graph will have length in range [1, 100].
+graph[i] will contain integers in range [0, graph.length - 1].
+graph[i] will not contain i or duplicate values.
+The graph is undirected: if any element j is in graph[i], then i will be in graph[j].
+*/
+class _0785_IsGraphBipartitle {
+private:
+    std::vector<int> colored;
+public:
+    bool isBipartitle(std::vector<std::vector<int>>& graph);
+    bool Color(const std::vector<std::vector<int>>& graph, int I, int color);
+}
+
+/*
 Given a directed, acyclic graph of N nodes.  Find all possible paths 
 from node 0 to node N-1, and return them in any order.
 
