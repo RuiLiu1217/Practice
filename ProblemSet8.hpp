5d2a18ebfc70218ab2b66f42206224b08d636b25
Rui Liu <liurui1217@gmail.com>
2020-01-28 15:33:20 -0500

    update

--- ProblemSet8.hpp
+++ ProblemSet8.hpp
@@ -270,6 +270,28 @@ public:
 };
 
 /*
+On a staircase, the i-th step has some non-negative cost cost[i] assigned 
+(0 indexed). Once you pay the cost, you can either climb one or two steps. 
+You need to find minimum cost to reach the top of the floor, and you can 
+either start from the step with index 0, or the step with index 1.
+
+Input: cost = [10, 15, 20]
+Output: 15
+Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+
+Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+Output: 6
+Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+Note:
+cost will have a length in the range [2, 1000].
+Every cost[i] will be an integer in the range [0, 999].
+*/
+class _0746_MinCostClimbingStairs {
+public:
+    int minCostClimbingStairs(std::vector<int>& cost);
+};
+
+/*
 You have a lock in front of you with 4 circular wheels. Each wheel has 10 slots: 
 '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'. The wheels can rotate freely and 
 wrap around: for example we can turn '9' to be '0', or '0' to be '9'. Each move 
