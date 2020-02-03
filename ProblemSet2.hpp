b1f63d4b5301b6d8d72f14e82a193d454bf6c86e
Rui Liu <liurui1217@gmail.com>
2020-02-03 00:51:14 -0500

    up

--- ProblemSet2.hpp
+++ ProblemSet2.hpp
@@ -389,6 +389,84 @@ public:
 };
 
 /*
+117. Populating Next Right Pointers in Each Node II
+Medium
+
+1235
+
+167
+
+Add to List
+
+Share
+Given a binary tree
+
+
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+
+ 
+
+Follow up:
+
+You may only use constant extra space.
+Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
+ 
+
+Example 1:
+
+
+
+Input: root = [1,2,3,4,5,null,7]
+Output: [1,#,2,3,#,4,5,7,#]
+Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+ 
+
+Constraints:
+
+The number of nodes in the given tree is less than 6000.
+-100 <= node.val <= 100
+*/
+class _0117_PopulatingNextRightPointersInEachNodeII {
+public:
+    struct Node {
+    int val;
+    Node *left;
+    Node *right;
+    Node *next;
+    };
+
+    Node* connect(Node* root) {
+        if(root == nullptr) {
+            return nullptr;
+        }
+        Node* p = root;
+        std::queue<std::pair<Node*, int>> q;
+        q.push({p, 0});
+        while(!q.empty()) {
+            const int N = q.size();
+            for(int i = 0; i < N; ++i) {
+                auto a = q.front();
+                auto t = a.first;
+                
+                q.pop();
+                if(!q.empty() && a.second == q.front().second) {
+                    t->next = q.front().first;
+                }
+                if(t->left) {
+                    q.push({t->left, a.second + 1});
+                }
+                if(t->right) {
+                    q.push({t->right, a.second + 1});
+                }
+            }
+        }
+        return root;
+    }
+};
+
+/*
 Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
 
 
--- _0116_PopulateNextRightPointerInEachNode.cpp
+++ _0116_PopulateNextRightPointerInEachNode.cpp
@@ -3,14 +3,15 @@
 
 void LeetCode::_0116_PopulatingNextRightPointerInEachNode::connect(Node* root) {
     if(root==nullptr) {
-        return;
+        return nullptr;
     }
     if(root->left != nullptr && root->right != nullptr) {
         root->left->next = root->right;
         if(root->next != nullptr) {
             root->right->next = root->next->left;
         }
-        connect(root->left);
-        connect(root->right);
+        root->left = connect(root->left);
+        root->right = connect(root->right);
     }
+    return root;
 }
