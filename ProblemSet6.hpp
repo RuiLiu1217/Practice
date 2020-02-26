f29dfb11c720bae1bbba7fb91b6ba0b9b82e3957
Rui Liu <liurui1217@gmail.com>
2020-02-25 22:29:32 -0500

    update

--- ProblemSet6.hpp
+++ ProblemSet6.hpp
@@ -208,6 +208,30 @@ public:
     std::string reverseStr(std::string s, int k);
 };
 
+/*
+Given a binary tree, you need to compute the length of the diameter 
+of the tree. The diameter of a binary tree is the length of the 
+longest path between any two nodes in a tree. This path may or may 
+not pass through the root.
+
+Given a binary tree
+          1
+         / \
+        2   3
+       / \     
+      4   5    
+Return 3, which is the length of the path [4,2,1,3] or [5,2,1,3].
+Note: The length of path between two nodes is represented by the number 
+of edges between them.
+*/
+class _0543_DiameterofBinaryTree {
+private:
+    int res;
+    int depth(TreeNode<int>* root);
+public:
+    int diameterOfBinaryTree(TreeNode<int>* root);
+
+};
 
 /*
 553. Optimal Division
