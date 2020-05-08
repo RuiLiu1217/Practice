99df25d57cb992e920e2a01318d00f20ee3977cb
Rui Liu <liurui1217@gmail.com>
2020-05-08 01:41:48 -0400

    Update _0223_RectangleArea.cpp

--- _0223_RectangleArea.cpp
+++ _0223_RectangleArea.cpp
@@ -3,5 +3,5 @@
 int LeetCode::_0223_RectangleArea::computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
     int x = std::min(G, C) > std::max(E, A) ? (std::min(G, C) - std::max(E, A)) : 0;
     int y = std::min(D, H) > std::max(B, F) ? (std::min(D, H) - std::max(B, F)) : 0;
-    return (D - B) * (C - A) + ((G - E) * (H - F) - x * y);
+    return (D - B) * (C - A) + ((G - E) * (H - F) - x * y); // calculate minus first to avoid overflow
 }
