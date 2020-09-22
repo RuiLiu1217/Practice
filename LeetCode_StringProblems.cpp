fee9aa03cc650f92d1faf577e2f02ffb58a8417a
Rui Liu <liurui1217@gmail.com>
2020-09-22 01:18:26 -0400

    update

--- LeetCode_StringProblems.cpp
+++ LeetCode_StringProblems.cpp
@@ -1,6 +1,7 @@
 #include "LeetCode_StringProblems.hpp"
 #include <functional>
 #include <climits>
+#include <numeric>
 #include <algorithm>
 #include <cctype>
 #include <stack>
@@ -984,4 +985,23 @@ std::vector<int> LC::_0241_DifferentWaysToAddParentheses::diffWaysToComp(const s
         }
     }
     return map[input.substr(start, end-start)] = res; // put the evaluated value to a map
+}
+
+int LC::_0467_UniqueSubstringsInWraparoundString::findSubstringInWraproundString(std::string p) {
+    std::vector<int> cnt(26, 0);
+    int len = 0;
+    for (int i = 0; i < p.size(); ++i) {
+        if (i > 0 && (p[i] == p[i - 1] + 1 || p[i - 1] - p[i] == 25)) {
+            ++len;
+        } else {
+            len = 1;
+        }
+        // Inspired by one trick!
+        // 我们看abcd这个字符串，以d结尾的子字符串有abcd, bcd, cd, d，
+        // 那么我们可以发现bcd或者cd这些以d结尾的字符串的子字符串都包含
+        // 在abcd中，那么我们知道以某个字符结束的最大字符串包含其他以该
+        // 字符结束的字符串的所有子字符串
+        cnt[p[i] - 'a'] = std::max(cnt[p[i] - 'a'], len);
+    }
+    return std::accumulate(cnt.begin(), cnt.end(), 0);
 }
--- LeetCode_StringProblems.hpp
+++ LeetCode_StringProblems.hpp
@@ -576,6 +576,32 @@ public:
     bool canPermutePalindrome(std::string s);
 };
 
+/*
+Consider the string s to be the infinite wraparound string of "abcdefghijklmnopqrstuvwxyz", 
+so s will look like this: "...zabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcd....".
+Now we have another string p. Your job is to find out how many unique non-empty substrings of 
+p are present in s. In particular, your input is the string p and you need to output the number 
+of different non-empty substrings of p in the string s.
+
+Note: p consists of only lowercase English letters and the size of p might be over 10000.
+
+Input: "a"
+Output: 1
+
+Explanation: Only the substring "a" of string "a" is in the string s.
+
+Input: "cac"
+Output: 2
+Explanation: There are two substrings "a", "c" of string "cac" in the string s.
+
+Input: "zab"
+Output: 6
+Explanation: There are six substrings "z", "a", "b", "za", "ab", "zab" of string "zab" in the string s.
+*/
+class _0467_UniqueSubstringsInWraparoundString {
+public:
+    int findSubstringInWraproundString(std::string p);
+};
 
 /*
 During the NBA playoffs, we always arrange the rather strong team to play with the rather weak team, like make the rank 1 team play with the rank nth team, which is a good strategy to make the contest more interesting. Now, you're given n teams, you need to output their final contest matches in the form of a string.
